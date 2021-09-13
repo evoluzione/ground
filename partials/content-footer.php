dac7caf7aa965364ebb8bc8a169ec43f1a7329dc
Valerio Acerbis <valerio.acerbis@evoluzionetelematica.it>
2021-09-13 14:59:26 +0200

    Fix color footer

--- partials/content-footer.php
+++ partials/content-footer.php
@@ -1,4 +1,4 @@
-<footer class="relative pt-16 bg-body-secondary lg:bg-transparent">
+<footer class="relative pt-16 bg-body-secondary">
 	<div class="container">
 		<div class="grid grid-cols-2 lg:grid-cols-4 gap-6">
 			<?php $locations = get_nav_menu_locations(); ?>
@@ -46,69 +46,72 @@
 		</div>
 	</div>
 
-	<div class="container mt-6 lg:flex lg:items-center lg:justify-start py-9 lg:pt-9 lg:space-x-9 bg-black lg:bg-transparent lg:border-line-primary lg:border-t">
-		<div class="mb-6 lg:mb-0">
-			<p class="text-center text-white lg:text-left lg:text-typo-primary text-sm lg:text-base">
-			<?php
-			_e( 'All rights reserved - © copyright', 'ground' );
-			echo date( 'Y' );
-			echo ( '  ' );
-			?>
-			<?php echo GROUND_COMPANY_NAME ? GROUND_COMPANY_NAME . ' - ' : null; ?>
-			<?php echo GROUND_COMPANY_ADDRESS ? GROUND_COMPANY_ADDRESS . ' - ' : null; ?>
-			<?php echo GROUND_COMPANY_CAP ? GROUND_COMPANY_CAP : null; ?> <?php echo GROUND_COMPANY_CITY ? GROUND_COMPANY_CITY : null; ?>
-			<?php echo GROUND_COMPANY_PROVINCE ? ( '( ' . GROUND_COMPANY_PROVINCE . ' )' ) : null; ?>
-			<?php echo GROUND_COMPANY_COUNTRY ? GROUND_COMPANY_COUNTRY : null; ?>
-			<?php echo GROUND_COMPANY_PIVA ? ' - P.IVA:' . GROUND_COMPANY_PIVA : null; ?>
-			<?php echo GROUND_COMPANY_CF ? ' - C.F.:' . GROUND_COMPANY_CF : null; ?> </p>
-		</div>
-		<?php if ( GROUND_SOCIAL_LINKEDIN_URL || GROUND_SOCIAL_FACEBOOK_URL || GROUND_SOCIAL_INSTAGRAM_URL || GROUND_SOCIAL_YOUTUBE_URL ) : ?>
-		<div class="lg:border-l border-line-primary">
+	<div class="container">
+		<div class="mt-6 py-9 border-t border-line-primary lg:flex lg:items-center lg:justify-start lg:space-x-9 lg:pt-9">			
+			<div class="mb-6 lg:mb-0">
+				<p class="text-center text-white lg:text-left lg:text-typo-primary text-sm lg:text-base">
+				<?php
+				_e( 'All rights reserved - © copyright', 'ground' );
+				echo date( 'Y' );
+				echo ( '  ' );
+				?>
+				<?php echo GROUND_COMPANY_NAME ? GROUND_COMPANY_NAME . ' - ' : null; ?>
+				<?php echo GROUND_COMPANY_ADDRESS ? GROUND_COMPANY_ADDRESS . ' - ' : null; ?>
+				<?php echo GROUND_COMPANY_CAP ? GROUND_COMPANY_CAP : null; ?> <?php echo GROUND_COMPANY_CITY ? GROUND_COMPANY_CITY : null; ?>
+				<?php echo GROUND_COMPANY_PROVINCE ? ( '( ' . GROUND_COMPANY_PROVINCE . ' )' ) : null; ?>
+				<?php echo GROUND_COMPANY_COUNTRY ? GROUND_COMPANY_COUNTRY : null; ?>
+				<?php echo GROUND_COMPANY_PIVA ? ' - P.IVA:' . GROUND_COMPANY_PIVA : null; ?>
+				<?php echo GROUND_COMPANY_CF ? ' - C.F.:' . GROUND_COMPANY_CF : null; ?> </p>
+			</div>
+			<?php if ( GROUND_SOCIAL_LINKEDIN_URL || GROUND_SOCIAL_FACEBOOK_URL || GROUND_SOCIAL_INSTAGRAM_URL || GROUND_SOCIAL_YOUTUBE_URL ) : ?>
+			<div class="lg:border-l border-line-primary">
 
-			<div class="lg:flex items-center justify-center lg:space-x-6">
+				<div class="lg:flex items-center justify-center lg:space-x-6">
 
-				<p class="pr-3 text-white text-center mb-2 text-sm lg:text-base lg:pl-6 lg:text-left lg:text-typo-primary lg:mb-0"><?php _e( 'Seguici sui social network', 'ground' ); ?></p>
+					<p class="pr-3 text-white text-center mb-2 text-sm lg:text-base lg:pl-6 lg:text-left lg:text-typo-primary lg:mb-0"><?php _e( 'Seguici sui social network', 'ground' ); ?></p>
 
-				<div class="flex justify-center lg:justify-start space-x-3">
-					<?php if ( GROUND_SOCIAL_LINKEDIN_URL ) : ?>
-					<span class="inline-block">
-						<a class="h-10 w-10 rounded-full text-white bg-blue-600 hover:text-white flex items-center" href="<?php echo GROUND_SOCIAL_LINKEDIN_URL; ?>">
-							<?php ground_icon( 'linkedin', 'mx-auto h-3' ); ?>
-						</a>
-					</span>
-					<?php endif; ?>
+					<div class="flex justify-center lg:justify-start space-x-3">
+						<?php if ( GROUND_SOCIAL_LINKEDIN_URL ) : ?>
+						<span class="inline-block">
+							<a class="h-10 w-10 rounded-full text-white bg-blue-600 hover:text-white flex items-center" href="<?php echo GROUND_SOCIAL_LINKEDIN_URL; ?>">
+								<?php ground_icon( 'linkedin', 'mx-auto h-3' ); ?>
+							</a>
+						</span>
+						<?php endif; ?>
 
-					<?php if ( GROUND_SOCIAL_FACEBOOK_URL ) : ?>
-					<span class="inline-block">
-						<a class="h-10 w-10 rounded-full text-white bg-blue-500 hover:text-white flex items-center" href="<?php echo GROUND_SOCIAL_FACEBOOK_URL; ?>">
-							<?php ground_icon( 'facebook', 'mx-auto h-4' ); ?>
-						</a>
-					</span>
-					<?php endif; ?>
+						<?php if ( GROUND_SOCIAL_FACEBOOK_URL ) : ?>
+						<span class="inline-block">
+							<a class="h-10 w-10 rounded-full text-white bg-blue-500 hover:text-white flex items-center" href="<?php echo GROUND_SOCIAL_FACEBOOK_URL; ?>">
+								<?php ground_icon( 'facebook', 'mx-auto h-4' ); ?>
+							</a>
+						</span>
+						<?php endif; ?>
 
-					<?php if ( GROUND_SOCIAL_INSTAGRAM_URL ) : ?>
-					<span class="inline-block">
-						<a class="h-10 w-10 rounded-full text-white bg-pink-500 p-2 hover:text-white flex items-center" href="<?php echo GROUND_SOCIAL_INSTAGRAM_URL; ?>">
-							<?php ground_icon( 'instagram', 'mx-auto h-4' ); ?>
-						</a>
-					</span>
-					<?php endif; ?>
+						<?php if ( GROUND_SOCIAL_INSTAGRAM_URL ) : ?>
+						<span class="inline-block">
+							<a class="h-10 w-10 rounded-full text-white bg-pink-500 p-2 hover:text-white flex items-center" href="<?php echo GROUND_SOCIAL_INSTAGRAM_URL; ?>">
+								<?php ground_icon( 'instagram', 'mx-auto h-4' ); ?>
+							</a>
+						</span>
+						<?php endif; ?>
 
-					<?php if ( GROUND_SOCIAL_YOUTUBE_URL ) : ?>
-					<span class="inline-block">
-						<a class="h-10 w-10 rounded-full text-white bg-red-500 p-2 hover:text-white flex items-center" href="<?php echo GROUND_SOCIAL_YOUTUBE_URL; ?>">
-							<?php ground_icon( 'youtube', 'mx-auto' ); ?>
-						</a>
-					</span>
-					<?php endif; ?>
+						<?php if ( GROUND_SOCIAL_YOUTUBE_URL ) : ?>
+						<span class="inline-block">
+							<a class="h-10 w-10 rounded-full text-white bg-red-500 p-2 hover:text-white flex items-center" href="<?php echo GROUND_SOCIAL_YOUTUBE_URL; ?>">
+								<?php ground_icon( 'youtube', 'mx-auto' ); ?>
+							</a>
+						</span>
+						<?php endif; ?>
 
+
+					</div>
 
 				</div>
 
 			</div>
+			<?php endif; ?>
 
 		</div>
-		<?php endif; ?>
 
 	</div>
 
