5614a127a630d62e4451068ca11dac6bd3b266c8
Stefano Cerri <stefano.cerri@evoluzionetelematica.it>
2021-10-12 11:57:31 +0200

    KNTMAIN-168

--- partials/headers/header-logo-centered.php
+++ partials/headers/header-logo-centered.php
@@ -1,11 +1,11 @@
 <?php get_template_part( 'partials/meassage', 'alert' ); ?>
 
-<header class="header header-logo-centered w-full z-30 bg-white dark:bg-black">
+<header class="header header-logo-centered w-full z-30 bg-body-primary">
 
-	<div class="bg-white dark:bg-black h-16 w-full z-30 lg:hidden">
-		<a class="js-back absolute mt-5 left-4 header__back cursor-pointer"> <span> <?php ground_icon( 'chevron-left', 'text-black dark:text-white' ); ?> </span> <?php _e( 'Indietro', 'ground' ); ?> </a>
+	<div class="bg-body-primary h-16 w-full z-30 lg:hidden">
+		<a class="js-back absolute mt-5 left-4 header__back cursor-pointer"> <span> <?php ground_icon( 'chevron-left', 'text-typo-primary' ); ?> </span> <?php _e( 'Indietro', 'ground' ); ?> </a>
 
-		<div class="header__bar-mobile container py-2 bg-white dark:bg-black grid grid-cols-12 items-center lg:flex lg:items-center lg:justify-between">
+		<div class="header__bar-mobile container py-2 bg-body-primary grid grid-cols-12 items-center lg:flex lg:items-center lg:justify-between">
 			<div class="col-span-4">
 				<?php get_template_part( 'partials/navicon', 'primary' ); ?>
 			</div>
@@ -14,7 +14,7 @@
 			</div>
 			<div class="col-span-4 flex justify-end items-center">
 				<button class="js-toggle js-cursor-hover mr-6 lg:mr-12" data-toggle-target=".search html" data-toggle-class-name="is-search-open">
-					<?php ground_icon( 'search', 'icon--filled text-black dark:text-white' ); ?>
+					<?php ground_icon( 'search', 'icon--filled text-typo-primary' ); ?>
 				</button>
 				<div class="header__cart relative py-4 col-span-4 flex justify-end items-center lg:hidden">
 					<?php get_template_part( 'partials/woocommerce/shopping-cart' ); ?>
@@ -25,7 +25,7 @@
 
 	</div>
 
-	<div class="js-menu-body header__body fixed left-0 pb-72 h-full w-screen z-40 bg-white dark:bg-black overflow-y-scroll lg:pt-0 lg:mt-0 lg:relative lg:top-auto lg:left-auto lg:bottom-auto lg:right-auto lg:bg-transparent lg:overflow-y-visible lg:w-full lg:pb-0">
+	<div class="js-menu-body header__body fixed left-0 pb-72 h-full w-screen z-40 bg-body-primary overflow-y-scroll lg:pt-0 lg:mt-0 lg:relative lg:top-auto lg:left-auto lg:bottom-auto lg:right-auto lg:bg-transparent lg:overflow-y-visible lg:w-full lg:pb-0">
 		<div class="js-menu-container header__container relative">
 
 			<div class="container lg:relative lg:grid lg:grid-cols-12 lg:items-center lg:h-32">
@@ -53,12 +53,12 @@
 					<?php endif; ?>
 
 					<button class="js-toggle lg:block lg:pr-3 lg:pl-3 lg:ml-3 lg:border-l lg:border-gray-200" data-toggle-target="#js-search-form html" data-toggle-class-name="is-search-open">
-						<?php ground_icon( 'search', 'icon--filled text-black dark:text-white' ); ?>
+						<?php ground_icon( 'search', 'icon--filled text-typo-primary' ); ?>
 					</button>
 
 					<?php if ( ! function_exists( 'is_woocommerce_activated' ) ) : ?>
 						<ul class=" relative z-0 lg:flex lg:items-center lg:space-x-5 lg:justify-end lg:m-0">
-							<li class="text-lg lg:border-l lg:border-gray-200 lg:pl-3 lg:text-base"><a class="inline-block lg:py-auto" href="<?php echo get_permalink( wc_get_page_id( 'myaccount' ) ); ?>"><?php ground_icon( 'user', 'icon--filled text-black dark:text-white' ); ?></a></li>
+							<li class="text-lg lg:border-l lg:border-gray-200 lg:pl-3 lg:text-base"><a class="inline-block lg:py-auto" href="<?php echo get_permalink( wc_get_page_id( 'myaccount' ) ); ?>"><?php ground_icon( 'user', 'icon--filled text-typo-primary' ); ?></a></li>
 							<li class="minicart-wrapper py-4 lg:inline-block"><?php get_template_part( 'partials/woocommerce/shopping-cart' ); ?> </li>
 						</ul>
 					<?php endif; ?>
