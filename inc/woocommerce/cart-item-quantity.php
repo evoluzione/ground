6d18278b49dde788b357d09c7eeb913ff73da80b
Matteo Tortelli <matteo.tortelli@evoluzione.agency>
2021-10-22 10:31:00 +0200

    KNTMAIN-158: fix bundle input value on click btn plus and minus

--- inc/woocommerce/cart-item-quantity.php
+++ inc/woocommerce/cart-item-quantity.php
@@ -3,51 +3,59 @@
 /**
  * Automatically Update Cart on Quantity Change
  */
-function ground_cart_refresh_update_qty() {
+function ground_cart_refresh_update_qty()
+{
 
-	if ( ! is_cart() && ! is_product() ) return;
+	if (!is_cart() && !is_product()) return;
 ?>
 
-		<script type="text/javascript">
+	<script type="text/javascript">
+		function clickButtonUpdate() {
+			const btnUpdate = jQuery('.woocommerce .actions button.button');
+			// enable button
+			btnUpdate.prop('disabled', false);
+			// click button
+			btnUpdate.trigger("click");
+		}
 
-			// onClick button plus and minus
-			jQuery(document).on('click','button.plus, button.minus', function(e){
-				
-				var qty = jQuery( this ).parent( '.quantity' ).find( '.qty' );
-				var val = parseFloat(qty.val());
-				var max = parseFloat(qty.attr( 'max' ));
-				var min = parseFloat(qty.attr( 'min' ));
-				var step = parseFloat(qty.attr( 'step' ));
-				
-				jQuery('.woocommerce .actions button.button').prop('disabled', false);
+		// onClick button plus and minus
+		jQuery(document).on('click', 'button.plus, button.minus', function(e) {
 
-				if ( jQuery( this ).is( '.plus' ) ) {
-					if ( max && ( max <= val ) ) {
-					qty.val( max );
-					} else {
-					qty.val( val + step );
-					}
+			var qty = jQuery(this).parent('.quantity').find('.qty');
+			var val = parseFloat(qty.val()) || 0;
+			var max = parseFloat(qty.attr('max'));
+			var min = parseFloat(qty.attr('min'));
+			var step = parseFloat(qty.attr('step'));
+
+
+			if (jQuery(this).is('.plus')) {
+				if (max && (max <= val)) {
+					qty.val(max);
 				} else {
-					if ( min && ( min >= val ) ) {
-					qty.val( min );
-					} else if ( val > 1 ) {
-					qty.val( val - step );
-					}
+					qty.val(val + step);
 				}
+			} else {
+				if (min && (min >= val)) {
+					qty.val(min);
+				} else if (val > 0) {
+					qty.val(val - step);
+				}
+			}
 
-				jQuery("[name='update_cart']").trigger("click");
-			});
+			clickButtonUpdate();
 
-			// onChange maual input value
-			var qty = jQuery( this ).parent( '.quantity' ).find( '.qty' );
-			jQuery(document).on('change', qty, function(){
-				jQuery("[name='update_cart']").trigger("click");
-			});
-      	</script>
+		});
+
+		// onChange maual input value
+		var qty = jQuery(this).parent('.quantity').find('.qty');
+		jQuery(document).on('change', qty, function() {
+			clickButtonUpdate();
+		});
+	</script>
 
 <?php
 }
 
-add_action( 'wp_footer', 'ground_cart_refresh_update_qty' );
+add_action('wp_footer', 'ground_cart_refresh_update_qty');
 
 ?>
