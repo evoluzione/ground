6ca77541e85e4c5a25a6f5507ac469af5750368f
Matteo Tortelli <matteo.tortelli@evoluzione.agency>
2021-11-02 09:54:19 +0100

    wp-config wp_end update

--- data/sample/wp-config-sample.php
+++ data/sample/wp-config-sample.php
@@ -24,7 +24,7 @@ switch ( getenv( 'WP_ENVIRONMENT_TYPE' ) ) {
 		define( 'WP_HOME', $protocol . $domain . '/path/to/wordpress' );
 		break;
 
-	case 'production':
+	default:
 		define( 'DB_NAME', 'database_name_here' );
 		define( 'DB_USER', 'username_here' );
 		define( 'DB_PASSWORD', 'password_here' );
