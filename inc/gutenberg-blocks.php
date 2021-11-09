038758a4c26742ac92f108bdd2701d724928c8e5
Matteo Tortelli <matteo.tortelli@evoluzione.agency>
2021-11-09 13:06:04 +0100

    KNTMAIN-173: aggiunta della classe custom ai blocchi nativi di gutenberg che non li avevano

--- inc/gutenberg-blocks.php
+++ inc/gutenberg-blocks.php
@@ -14,3 +14,81 @@ add_action(
 		wp_enqueue_script( 'ground-gutenberg-registerblocktype', get_template_directory_uri() . '/src/blocks-registerBlockType.js', $deps );
 	}
 );
+
+
+function ground_wp_blocks_add_custom_class( string $block_content, array $block ) {
+
+	$has_class       = strpos( $block_content, 'class="' );
+	$class_paragraph = 'wp-block-paragraph';
+	$class_heading   = 'wp-block-heading';
+	$class_list      = 'wp-block-list';
+
+	/**
+	 * Paragraph
+	 */
+	if ( $block['blockName'] === 'core/paragraph' ) {
+		return handle_function_class_paragraph( $block_content, $has_class, $class_paragraph );
+	}
+
+	/**
+	 * Heading
+	 */
+	if ( $block['blockName'] === 'core/heading' ) {
+		return handle_function_class_heading( $block_content, $has_class, $class_heading );
+	}
+
+	/**
+	 * List
+	 */
+	if ( $block['blockName'] === 'core/list' ) {
+		return handle_function_class_list( $block_content, $has_class, $class_list );
+	}
+
+	return $block_content;
+}
+add_filter( 'render_block', 'ground_wp_blocks_add_custom_class', 10, 2 );
+
+/**
+ * Function to add a custom class to paragraph block
+ *
+ * @param string  $block_content Is the block to handle.
+ * @param boolean $has_class It detect if the block has a class.
+ * @param string  $class_to_add Is the class to add.
+ * @return string
+ */
+function handle_function_class_paragraph( $block_content, $has_class, $class_to_add ) {
+	// Add class attribute.
+	$new_block_content = preg_replace( '/<p>/i', '<p class="">', $block_content );
+	// Appen custom class.
+	return preg_replace( '/class="(.*)"/', 'class="' . $class_to_add . ( $has_class ? ' ' : '' ) . '$1"', $new_block_content );
+}
+
+/**
+ * Function to add a custom class to heading block
+ *
+ * @param string  $block_content Is the block to handle.
+ * @param boolean $has_class It detect if the block has a class.
+ * @param string  $class_to_add Is the class to add.
+ * @return string
+ */
+function handle_function_class_heading( $block_content, $has_class, $class_to_add ) {
+	// Add class attribute.
+	$new_block_content = preg_replace( '/<h(.)>/i', '<h$1 class="">', $block_content );
+	// Appen custom class.
+	return preg_replace( '/class="(.*)"/', 'class="' . $class_to_add . ( $has_class ? ' ' : '' ) . '$1"', $new_block_content );
+}
+
+/**
+ * Function to add a custom class to list block
+ *
+ * @param string  $block_content Is the block to handle.
+ * @param boolean $has_class It detect if the block has a class.
+ * @param string  $class_to_add Is the class to add.
+ * @return string
+ */
+function handle_function_class_list( $block_content, $has_class, $class_to_add ) {
+	// Add class attribute.
+	$new_block_content = preg_replace( '/<(ol|ul)>/i', '<$1 class="">', $block_content );
+	// Appen custom class.
+	return preg_replace( '/class="(.*)"/', 'class="' . $class_to_add . ( $has_class ? ' ' : '' ) . '$1"', $new_block_content );
+}
--- src/blocks-getSaveContent-extraProps.js
+++ src/blocks-getSaveContent-extraProps.js
@@ -1,17 +1,17 @@
 /**
  * setExtraPropsToBlockType
  */
-function setExtraPropsToBlockType(props, blockType, attributes) {
-	const notDefined = typeof props.className === 'undefined' || !props.className ? true : false;
+// function setExtraPropsToBlockType(props, blockType, attributes) {
+// 	const notDefined = typeof props.className === 'undefined' || !props.className ? true : false;
 
-	if (blockType.name === 'core/list') {
-		return Object.assign(props, { class: notDefined ? 'post__list' : `post__list ${props.className}` });
-	}
+// 	if (blockType.name === 'core/list') {
+// 		return Object.assign(props, { class: notDefined ? 'post__list' : `post__list ${props.className}` });
+// 	}
 
-	return props;
-}
+// 	return props;
+// }
 
 /**
  * https://developer.wordpress.org/block-editor/reference-guides/filters/block-filters/#blocks-getsavecontent-extraprops
  */
-wp.hooks.addFilter('blocks.getSaveContent.extraProps', 'ground/block-filters', setExtraPropsToBlockType);
+// wp.hooks.addFilter('blocks.getSaveContent.extraProps', 'ground/block-filters', setExtraPropsToBlockType);
