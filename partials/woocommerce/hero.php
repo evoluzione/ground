447b5ce00c7af175e72ad099272df266d679826d
Stefano Cerri <stefano.cerri@evoluzionetelematica.it>
2021-11-20 09:57:41 +0100

    update hero

--- partials/woocommerce/hero.php
+++ partials/woocommerce/hero.php
@@ -1,36 +1,31 @@
 <?php
-	$page_id             = get_queried_object();
-	$category_highlights = get_field( 'category_highlights', $page_id );
+if ( is_product_category() ) {
+	$page_id = get_queried_object();
+} else {
+	$page_id = wc_get_page_id( 'shop' );
+}
+
+$category_highlights = get_field( 'category_highlights', $page_id );
 
 if ( $category_highlights ) :
 	$repeater_count = count( get_field( 'category_highlights', $page_id ) );
 	endif;
 ?>
 
-
-<?php if ( is_product_category() ) { ?>
-	<?php	// Pagina categoria catalogo ?>
-
 	<div class="hero hero--full-width">
 
 		<div class="hero__body">
 		<?php
+		if ( is_product_category() ) {
 			$ground_cat   = get_queried_object();
 			$thumbnail_id = get_term_meta( $ground_cat->term_id, 'thumbnail_id', true );
-		if ( $thumbnail_id ) {
-			?>
-				<img class="hero__image" loading="lazy" src="<?php echo wp_get_attachment_image_src( $thumbnail_id, 'large' )[0]; ?>">
-					<div class="hero__image-filter" aria-hidden="true"></div>
-				<div class="hero__content hero-has-thumbnail">
-				<?php
 		} else {
-			?>
-				<div class="hero__content">
-
-			<?php
+			$thumbnail_id = get_post_thumbnail_id( wc_get_page_id( 'shop' ) );
 		}
 		?>
-
+		<img class="hero__image" loading="lazy" src="<?php echo wp_get_attachment_image_src( $thumbnail_id, 'large' )[0]; ?>">
+		<div class="hero__image-filter" aria-hidden="true"></div>
+		<div class="hero__content text-typo-primary">
 			<div class="grid grid-cols-12 gap-x-6 <?php echo $repeater_count == 1 ? 'lg:items-center' : ''; ?>">
 				<div class="col-span-full lg:col-span-5">
 					<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
@@ -172,41 +167,3 @@ if ( $category_highlights ) :
 
 </div>
 
-<?php } else { ?>
-	<?php	// es. Pagina Shop ?>
-	<div class="hero hero--full-width">
-
-		<div class="relative">
-			<?php
-				$page_id      = get_option( 'woocommerce_shop_page_id' );
-				$thumbnail_id = get_post_thumbnail_id( $page_id );
-			if ( $thumbnail_id ) {
-				?>
-					<img class="hero__image" loading="lazy" src="<?php echo wp_get_attachment_image_src( $thumbnail_id, 'large' )[0]; ?>">
-					<div class="hero__image-filter" aria-hidden="true"></div>
-					<div class="hero__content hero-has-thumbnail">
-					<?php
-			} else {
-				?>
-					<div class="container my-12 lg:my-24 border-b border-line-primary">
-				<?php
-			}
-			?>
-
-				<div class="grid grid-cols-12 gap-x-6">
-					<div class="col-span-full lg:col-span-3">
-						<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
-							<h1 class="hero__title mb-6"><?php woocommerce_page_title(); ?></h1>
-						<?php endif; ?>
-					</div>
-					<div class="col-span-full lg:col-start-6 lg:col-span-7">
-						<div class="hero__excerpt">
-							<?php do_action( 'woocommerce_archive_description' ); ?>
-						</div>
-					</div>
-				</div>
-			</div>
-
-		</div>
-	</div>
-<?php } ?>
