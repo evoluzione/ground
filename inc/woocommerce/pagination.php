d1b09fe33806d507057e2f74ed9328bc5030c2c1
Valerio Acerbis <valerio.acerbis@evoluzionetelematica.it>
2021-11-25 12:38:06 +0100

    20 post x page : pagination woocommerce

--- inc/woocommerce/pagination.php
+++ inc/woocommerce/pagination.php
@@ -21,8 +21,8 @@ add_action( 'woocommerce_after_shop_loop', 'ground_woocommerce_pagination', 10 )
  * @param int $cols Contains the current number of products per page based on the value stored on Options -> Reading.
  */
 function ground_woocommerce_products_per_page( $cols ) {
-	$cols = 9;
+	$cols = 20;
 	return $cols;
 }
 
-// add_filter( 'loop_shop_per_page', 'ground_woocommerce_products_per_page', 20 );
+add_filter( 'loop_shop_per_page', 'ground_woocommerce_products_per_page', 20 );
