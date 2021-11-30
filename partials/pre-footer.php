edd65d91c23fe3b6d5dd99633103a7f60c5657ba
Valerio Acerbis <valerio.acerbis@evoluzionetelematica.it>
2021-11-30 17:23:20 +0100

    pre-footer: remove rounded from payments icons

--- partials/pre-footer.php
+++ partials/pre-footer.php
@@ -16,7 +16,7 @@ $repeater = GROUND_SHOP_PAYMENT;
 				<?php if ( $repeater ) : ?>
 					<?php foreach ( $repeater as $row ) : ?>
 					<div class="p-2 flex justify-center border border-line-primary rounded">
-						<img class="object-contain w-12 h-5" src="<?php echo GROUND_TEMPLATE_URL . '/img/getway-icons/' . $row . '.svg'; ?>"/>
+						<img class="object-contain w-12 h-5 rounded-none" src="<?php echo GROUND_TEMPLATE_URL . '/img/getway-icons/' . $row . '.svg'; ?>"/>
 					</div>
 				<?php endforeach; ?>
 			<?php endif; ?>
