adca82e2c79a7188708227c7fdacd01443b14904
Valerio Acerbis <valerio.acerbis@evoluzionetelematica.it>
2021-11-30 16:57:36 +0100

    minor fix pre footer

--- partials/pre-footer.php
+++ partials/pre-footer.php
@@ -6,15 +6,15 @@ $repeater = GROUND_SHOP_PAYMENT;
  <div class="container my-12 lg:my-24">
 	<div class="text-center mb-24 grid grid-cols-12 gap-6 lg:text-left">
 		<div class="col-span-full lg:col-span-6">
-			<?php if(GROUND_PAYMENTS_TITLE) : ?>
+			<?php if ( GROUND_PAYMENTS_TITLE ) : ?>
 				<div class="text-xl font-bold mb-4">
 					<?php echo esc_html( GROUND_PAYMENTS_TITLE ); ?>
 				</div>
 			<?php endif; ?>
 
-			<div class="flex flex-wrap gap-4 justify-start mb-3">
+			<div class="flex flex-wrap gap-4 justify-center lg:justify-start mb-3">
 				<?php if ( $repeater ) : ?>
-				<?php foreach ( $repeater as $row ) : ?>
+					<?php foreach ( $repeater as $row ) : ?>
 					<div class="p-2 flex justify-center border border-line-primary rounded">
 						<img class="object-contain w-12" src="<?php echo GROUND_TEMPLATE_URL . '/img/getway-icons/' . $row . '.svg'; ?>"/>
 					</div>
@@ -22,8 +22,8 @@ $repeater = GROUND_SHOP_PAYMENT;
 			<?php endif; ?>
 			</div>
 
-			<?php if(GROUND_PAYMENTS_CONTENT) : ?>
-				<div class="text-typo-secondary md:max-w-lg">
+			<?php if ( GROUND_PAYMENTS_CONTENT ) : ?>
+				<div class="text-typo-secondary lg:pr-44">
 					<?php echo esc_html( GROUND_PAYMENTS_CONTENT ); ?>
 				</div>
 			<?php endif; ?>
@@ -36,7 +36,7 @@ $repeater = GROUND_SHOP_PAYMENT;
 				<div class="text-xl font-bold mb-3">
 					<?php echo esc_html( GROUND_SHIPPING_TITLE ); ?>
 				</div>
-				<div class="text-typo-secondary md:max-w-lg">
+				<div class="text-typo-secondary lg:pr-44">
 					<?php echo esc_html( GROUND_SHIPPING_CONTENT ); ?>
 				</div>
 			</div>
