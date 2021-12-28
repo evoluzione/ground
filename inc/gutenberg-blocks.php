3acd2a48404dc262cb79bbc8ea31c98bd4b3f342
Matteo Tortelli <matteo.tortelli@evoluzione.agency>
2021-12-28 18:08:09 +0100

    fix widget deps error wp-editor

--- inc/gutenberg-blocks.php
+++ inc/gutenberg-blocks.php
@@ -7,11 +7,20 @@
 add_action(
 	'enqueue_block_assets',
 	function () {
-		$deps      = array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-components', 'wp-editor' );
+
+		global $pagenow;
+
+		// In widget page wp-editor is already a dependency.
+		if ($pagenow === 'widgets.php') {
+			$deps      = array('wp-blocks', 'wp-i18n', 'wp-element', 'wp-components');
+		} else {
+			$deps      = array('wp-blocks', 'wp-i18n', 'wp-element', 'wp-components', 'wp-editor');
+		}
+
 		$path      = get_template_directory_uri() . '/gutenberg/dist/bundle.js';
 		$in_footer = true;
 
-		wp_enqueue_script( 'ground/gutenberg', $path, $deps, false, $in_footer );
+		wp_enqueue_script('ground/gutenberg', $path, $deps, false, $in_footer);
 	}
 );
 
@@ -22,9 +31,10 @@ add_action(
  * @param array  $block The object block.
  * @return string
  */
-function ground_wp_blocks_add_custom_class( string $block_content, array $block ) {
+function ground_wp_blocks_add_custom_class(string $block_content, array $block)
+{
 
-	$has_class       = strpos( $block_content, 'class="' );
+	$has_class       = strpos($block_content, 'class="');
 	$class_paragraph = 'wp-block-paragraph';
 	$class_heading   = 'wp-block-heading';
 	$class_list      = 'wp-block-list';
@@ -32,27 +42,27 @@ function ground_wp_blocks_add_custom_class( string $block_content, array $block 
 	/**
 	 * Paragraph
 	 */
-	if ( $block['blockName'] === 'core/paragraph' ) {
-		return handle_function_class_paragraph( $block_content, $has_class, $class_paragraph );
+	if ($block['blockName'] === 'core/paragraph') {
+		return handle_function_class_paragraph($block_content, $has_class, $class_paragraph);
 	}
 
 	/**
 	 * Heading
 	 */
-	if ( $block['blockName'] === 'core/heading' ) {
-		return handle_function_class_heading( $block_content, $has_class, $class_heading );
+	if ($block['blockName'] === 'core/heading') {
+		return handle_function_class_heading($block_content, $has_class, $class_heading);
 	}
 
 	/**
 	 * List
 	 */
-	if ( $block['blockName'] === 'core/list' ) {
-		return handle_function_class_list( $block_content, $has_class, $class_list );
+	if ($block['blockName'] === 'core/list') {
+		return handle_function_class_list($block_content, $has_class, $class_list);
 	}
 
 	return $block_content;
 }
-add_filter( 'render_block', 'ground_wp_blocks_add_custom_class', 10, 2 );
+add_filter('render_block', 'ground_wp_blocks_add_custom_class', 10, 2);
 
 /**
  * Function to add a custom class to paragraph block
@@ -62,11 +72,12 @@ add_filter( 'render_block', 'ground_wp_blocks_add_custom_class', 10, 2 );
  * @param string  $class_to_add Is the class to add.
  * @return string
  */
-function handle_function_class_paragraph( $block_content, $has_class, $class_to_add ) {
+function handle_function_class_paragraph($block_content, $has_class, $class_to_add)
+{
 	// Add class attribute.
-	$new_block_content = preg_replace( '/<p>/i', '<p class="">', $block_content );
+	$new_block_content = preg_replace('/<p>/i', '<p class="">', $block_content);
 	// Appen custom class.
-	return preg_replace( '/class="(.*)"/', 'class="' . $class_to_add . ( $has_class ? ' ' : '' ) . '$1"', $new_block_content );
+	return preg_replace('/class="(.*)"/', 'class="' . $class_to_add . ($has_class ? ' ' : '') . '$1"', $new_block_content);
 }
 
 /**
@@ -77,11 +88,12 @@ function handle_function_class_paragraph( $block_content, $has_class, $class_to_
  * @param string  $class_to_add Is the class to add.
  * @return string
  */
-function handle_function_class_heading( $block_content, $has_class, $class_to_add ) {
+function handle_function_class_heading($block_content, $has_class, $class_to_add)
+{
 	// Add class attribute.
-	$new_block_content = preg_replace( '/<h(.)>/i', '<h$1 class="">', $block_content );
+	$new_block_content = preg_replace('/<h(.)>/i', '<h$1 class="">', $block_content);
 	// Appen custom class.
-	return preg_replace( '/class="(.*)"/', 'class="' . $class_to_add . ( $has_class ? ' ' : '' ) . '$1"', $new_block_content );
+	return preg_replace('/class="(.*)"/', 'class="' . $class_to_add . ($has_class ? ' ' : '') . '$1"', $new_block_content);
 }
 
 /**
@@ -92,9 +104,10 @@ function handle_function_class_heading( $block_content, $has_class, $class_to_ad
  * @param string  $class_to_add Is the class to add.
  * @return string
  */
-function handle_function_class_list( $block_content, $has_class, $class_to_add ) {
+function handle_function_class_list($block_content, $has_class, $class_to_add)
+{
 	// Add class attribute.
-	$new_block_content = preg_replace( '/<(ol|ul)>/i', '<$1 class="">', $block_content );
+	$new_block_content = preg_replace('/<(ol|ul)>/i', '<$1 class="">', $block_content);
 	// Appen custom class.
-	return preg_replace( '/class="(.*)"/', 'class="' . $class_to_add . ( $has_class ? ' ' : '' ) . '$1"', $new_block_content );
+	return preg_replace('/class="(.*)"/', 'class="' . $class_to_add . ($has_class ? ' ' : '') . '$1"', $new_block_content);
 }
