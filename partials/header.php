679386e6c2ba08681ea8ea586ec68f3eb0572eb4
Matteo Tortelli <matteo.tortelli@evoluzione.agency>
2021-12-28 17:55:00 +0100

    fix check if wooc is active

--- partials/header.php
+++ partials/header.php
@@ -5,14 +5,14 @@
 	<meta charset="<?php echo GROUND_CHARSET; ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="theme-color" content="">
-	<?php if ( ! function_exists( 'has_site_icon' ) || ! has_site_icon() ) { ?>
+	<?php if (!function_exists('has_site_icon') || !has_site_icon()) { ?>
 		<link rel="icon" type="image/png" href="<?php echo GROUND_TEMPLATE_URL . '/img/favicon.png'; ?>">
 		<link rel="apple-touch-icon" href="<?php echo GROUND_TEMPLATE_URL . '/img/icon.png'; ?>">
 	<?php } ?>
 	<?php wp_head(); ?>
 </head>
 
-<body id="scroll-top" <?php body_class( 'font-primary bg-body-primary text-typo-primary debug-screens' ); ?> data-template-url="<?php echo GROUND_TEMPLATE_URL; ?>">
+<body id="scroll-top" <?php body_class('font-primary bg-body-primary text-typo-primary debug-screens'); ?> data-template-url="<?php echo GROUND_TEMPLATE_URL; ?>">
 
 	<?php
 	/**
@@ -20,24 +20,23 @@
 	 *
 	 * @hooked ground_woocommerce_minicart - 10
 	 */
-	do_action( 'ground_before_header' );
+	do_action('ground_before_header');
 
 	/**
 	 * Hook: ground_header.
 	 *
 	 * @hooked ground_header_type - 10
 	 */
-	do_action( 'ground_header' );
+	do_action('ground_header');
 
 	/**
 	 * Hook: ground_after_header.
 	 */
-	do_action( 'ground_after_header' );
+	do_action('ground_after_header');
 	?>
 
 	<div data-router-wrapper>
 
 		<div <?php ground_view_name(); ?>>
 
-			<main role="main" class="<?php echo is_cart() || is_checkout() || is_account_page() ? 'container' : esc_attr( GROUND_CONTAINER ); ?>">
-
+			<main role="main" class="<?php echo class_exists('WooCommerce') && (is_cart() || is_checkout() || is_account_page()) ? 'container' : esc_attr(GROUND_CONTAINER); ?>">
