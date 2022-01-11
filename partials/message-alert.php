2c06e286185c07a23d6c3523731bb4a7fe76c700
Stefano Cerri <stefano.cerri@evoluzionetelematica.it>
2022-01-11 16:49:40 +0100

    update blog

--- partials/message-alert.php
+++ partials/message-alert.php
@@ -1,17 +1,19 @@
 <?php if ( GROUND_HEADER_ADVICE_PRIMARY ) : ?>
 <div class="message-alert message-alert--primary px-6 <?php echo GROUND_HEADER_ADVICE_PRIMARY ? 'bg-body-secondary py-2' : ''; ?>">
-	<div class="relative h-10 text-center text-typo-primary">
-		<p class="message-alert__content text-typo-primary">
+	<div class="relative <?php echo GROUND_HEADER_ADVICE_SECONDARY ? 'h-10' : ''; ?> text-center text-typo-primary">
+		<p class="<?php echo GROUND_HEADER_ADVICE_SECONDARY ? 'message-alert__content text-typo-primary' : ''; ?>">
 			<?php echo GROUND_HEADER_ADVICE_PRIMARY; ?>
 		</p>
-
+		<?php if ( GROUND_HEADER_ADVICE_SECONDARY ) : ?>
 		<p class="message-alert__content text-typo-primary">
 			<?php echo GROUND_HEADER_ADVICE_SECONDARY; ?>
 		</p>
-
+		<?php endif; ?>
+		<?php if ( GROUND_HEADER_ADVICE_TERTIARY ) : ?>
 		<p class="message-alert__content text-typo-primary">
 			<?php echo GROUND_HEADER_ADVICE_TERTIARY; ?>
 		</p>
+		<?php endif; ?>
 	</div>
 </div>
 <?php endif; ?>
