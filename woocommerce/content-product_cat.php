5863e6f1f2c4f3be2284340eba467e7f2731627e
Valerio Acerbis <valerio.acerbis@evoluzionetelematica.it>
2022-01-27 10:58:09 +0100

    Aggiornamento versioni file Woo sovrascritti: non ci sono modifiche significative e sono stati aggiornate solo le versioni

--- woocommerce/content-product_cat.php
+++ woocommerce/content-product_cat.php
@@ -14,7 +14,7 @@
  * @see     https://docs.woocommerce.com/document/template-structure/
  * @author  WooThemes
  * @package WooCommerce/Templates
- * @version 2.6.1
+ * @version 4.7.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
