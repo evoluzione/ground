f44cfe22fcafa4b9a3eaaa09af8e7be237395ed5
Fabio Quarantini <fabio.quarantini@evoluzionetelematica.it>
2022-02-11 14:58:30 +0100

    Fix formatting

--- 404.php
+++ 404.php
@@ -8,20 +8,18 @@
 get_template_part( 'partials/header' );
 ?>
 
+	<article class="page page--404">
 
-		<article class="page page--404">
+		<header class="page__header">
+			<h1 class="page__title"><?php esc_html_e( 'Oops!', 'ground' ); ?></h1>
+		</header>
 
-			<header class="page__header">
-				<h1 class="page__title"><?php esc_html_e( 'Oops!', 'ground' ); ?></h1>
-			</header>
+		<div class="page__body">
+			<h3><?php esc_html_e( "The page you're looking for can't be found", 'ground' ); ?></h3>
+			<p><em><?php esc_html_e( 'Error code: 404', 'ground' ); ?></em></p>
+		</div> <!-- End .page__body -->
 
-			<div class="page__body">
-				<h3><?php esc_html_e( "The page you're looking for can't be found", 'ground' ); ?></h3>
-				<p><em><?php esc_html_e( 'Error code: 404', 'ground' ); ?></em></p>
-			</div> <!-- End .page__body -->
-
-		</article> <!-- End .page -->
-
+	</article> <!-- End .page -->
 
 <?php
 get_template_part( 'partials/footer' );
