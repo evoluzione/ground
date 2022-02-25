738b54ee4ac041fe609db66cd8c661dd37e9611d
Matteo Tortelli <matteo.tortelli@evoluzione.agency>
2022-02-25 12:51:29 +0100

    Fix handleAnimation getAnimation default

--- gutenberg/webpack.config.js
+++ gutenberg/webpack.config.js
@@ -29,7 +29,27 @@ const rules = [
 		test: /\.js$/,
 		include: [JS_DIR],
 		exclude: /node_modules/,
-		use: 'babel-loader'
+		use: 'babel-loader',
+		options: {
+			presets: [
+				  "@babel/preset-env",
+				  {
+					"targets": {
+					  "browsers": [
+						"last 2 Chrome versions",
+						"last 2 Firefox versions",
+						"last 2 Safari versions",
+						"last 2 iOS versions",
+						"last 1 Android version",
+						"last 1 ChromeAndroid version",
+						"ie 11"
+					  ]
+					}
+				  },
+				  "@babel/preset-react"
+				],
+			  
+		}
 	}
 ];
 
