eaaefbb685e4e6a82be9867403faea48fd4bade8
Matteo Tortelli <matteo.tortelli@evoluzione.agency>
2022-03-07 10:26:47 +0100

    KNTMAIN-311: Fix and refactor ground_wp_blocks_handle_custom_class

--- inc/gutenberg-blocks.php
+++ inc/gutenberg-blocks.php
@@ -26,89 +26,62 @@ if (is_admin()) {
 }
 
 /**
- * Function to add custom class to core blocks
+ * Function to handle custom class to core blocks
  *
  * @param string $block_content The content of the block.
  * @param array  $block The object block.
  * @return string
  */
-function ground_wp_blocks_add_custom_class(string $block_content, array $block)
+function ground_wp_blocks_handle_custom_class(string $block_content, array $block)
 {
 
-	$has_class       = strpos($block_content, 'class="');
-	$class_paragraph = 'wp-block-paragraph';
-	$class_heading   = 'wp-block-heading';
-	$class_list      = 'wp-block-list';
+	$has_class = strpos($block_content, 'class="');
 
 	/**
 	 * Paragraph
 	 */
 	if ($block['blockName'] === 'core/paragraph') {
-		return handle_function_class_paragraph($block_content, $has_class, $class_paragraph);
+		return ground_wp_blocks_add_custom_class($block_content, $has_class, 'wp-block-paragraph');
 	}
 
 	/**
 	 * Heading
 	 */
 	if ($block['blockName'] === 'core/heading') {
-		return handle_function_class_heading($block_content, $has_class, $class_heading);
+		return ground_wp_blocks_add_custom_class($block_content, $has_class, 'wp-block-heading');
 	}
 
 	/**
 	 * List
 	 */
 	if ($block['blockName'] === 'core/list') {
-		return handle_function_class_list($block_content, $has_class, $class_list);
+		return ground_wp_blocks_add_custom_class($block_content, $has_class, 'wp-block-list');
 	}
 
 	return $block_content;
 }
-add_filter('render_block', 'ground_wp_blocks_add_custom_class', 10, 2);
+add_filter('render_block', 'ground_wp_blocks_handle_custom_class', 10, 2);
 
 /**
- * Function to add a custom class to paragraph block
+ * Function to add a custom class to native wp blocks
  *
- * @param string  $block_content Is the block to handle.
- * @param boolean $has_class It detect if the block has a class.
- * @param string  $class_to_add Is the class to add.
+ * @param string  $block_content The content block to handle.
+ * @param boolean $has_class Detect if the block has already a class.
+ * @param string  $class_to_add The class to add.
  * @return string
  */
-function handle_function_class_paragraph($block_content, $has_class, $class_to_add)
+function ground_wp_blocks_add_custom_class($block_content, $has_class, $class_to_add)
 {
-	// Add class attribute.
-	$new_block_content = preg_replace('/<p>/i', '<p class="">', $block_content);
-	// Appen custom class.
-	return preg_replace('/class="(.*)"/', 'class="' . $class_to_add . ($has_class ? ' ' : '') . '$1"', $new_block_content);
-}
+	if (!$has_class) {
+		// Add class attribute.
+		// Detect paragraph, heading tag, ordered and unordered list
+		$pattern = '/<(p|h\d|[uo]l)(.*)/i';
+		$replacement = '<$1 class=""$2';
+		$block_content = preg_replace($pattern, $replacement, $block_content);
+	}
 
-/**
- * Function to add a custom class to heading block
- *
- * @param string  $block_content Is the block to handle.
- * @param boolean $has_class It detect if the block has a class.
- * @param string  $class_to_add Is the class to add.
- * @return string
- */
-function handle_function_class_heading($block_content, $has_class, $class_to_add)
-{
-	// Add class attribute.
-	$new_block_content = preg_replace('/<h(.)>/i', '<h$1 class="">', $block_content);
-	// Appen custom class.
-	return preg_replace('/class="(.*)"/', 'class="' . $class_to_add . ($has_class ? ' ' : '') . '$1"', $new_block_content);
-}
-
-/**
- * Function to add a custom class to list block
- *
- * @param string  $block_content Is the block to handle.
- * @param boolean $has_class It detect if the block has a class.
- * @param string  $class_to_add Is the class to add.
- * @return string
- */
-function handle_function_class_list($block_content, $has_class, $class_to_add)
-{
-	// Add class attribute.
-	$new_block_content = preg_replace('/<(ol|ul)>/i', '<$1 class="">', $block_content);
-	// Appen custom class.
-	return preg_replace('/class="(.*)"/', 'class="' . $class_to_add . ($has_class ? ' ' : '') . '$1"', $new_block_content);
+	// Append custom class.
+	$pattern = '/class="(.*)"/';
+	$replacement =  'class="' . $class_to_add . ($has_class ? ' ' : '') . '$1"';
+	return preg_replace($pattern, $replacement, $block_content);
 }
