01562186e3ff32339ac44080bf7eff021e1c4ff6
Valerio Acerbis <valerio.acerbis@evoluzionetelematica.it>
2022-03-18 14:02:30 +0100

    Hide shipping rates when free shipping is available, but keep "Local pickup"

--- inc/woocommerce/shipping.php
+++ inc/woocommerce/shipping.php
@@ -1,19 +1,31 @@
 <?php
 
 /**
- * Hide shipping rates when free shipping is available.
- *
- * @param array $rates Array of rates found for the package.
- * @return array
+ * Hide shipping rates when free shipping is available, but keep "Local pickup"
  */
-function ground_woocommerce__shipping_when_free_is_available( $rates ) {
-	$free = array();
+
+function ground_woocommerce_hide_shipping_when_free_is_available( $rates, $package ) {
+	$new_rates = array();
 	foreach ( $rates as $rate_id => $rate ) {
+		// Only modify rates if free_shipping is present.
 		if ( 'free_shipping' === $rate->method_id ) {
-			$free[ $rate_id ] = $rate;
+			$new_rates[ $rate_id ] = $rate;
 			break;
 		}
 	}
-	return ! empty( $free ) ? $free : $rates;
+
+	if ( ! empty( $new_rates ) ) {
+		// Save local pickup if it's present.
+		foreach ( $rates as $rate_id => $rate ) {
+			if ( 'local_pickup' === $rate->method_id ) {
+				$new_rates[ $rate_id ] = $rate;
+				break;
+			}
+		}
+		return $new_rates;
+	}
+
+	return $rates;
 }
-add_filter( 'woocommerce_package_rates', 'ground_woocommerce__shipping_when_free_is_available', 100 );
+
+add_filter( 'woocommerce_package_rates', 'ground_woocommerce_hide_shipping_when_free_is_available', 10, 2 );
