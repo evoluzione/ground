9a0812b7f2fbf2af80e2c40299db4fc953f791c1
Valerio Acerbis <valerio.acerbis@evoluzionetelematica.it>
2022-04-28 15:18:38 +0200

    Fix footer ( is-container-full/is-container-boxed )

--- template-parts/footer/footer-simple.php
+++ template-parts/footer/footer-simple.php
@@ -1,51 +1,56 @@
 <footer class="footer footer--simple relative pt-16 bg-senary">
-	<div class="container">
-		<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+	
+	<div class="footer__body">
 
-			<?php if ( is_active_sidebar( 'sidebar-footer-primary' ) ) : ?>
-				<div class="mb-12">
-					<?php get_template_part( 'template-parts/sidebar/sidebar-footer-primary' ); ?>
-				</div>
-			<?php endif; ?>
+		<div class="relative">
+			<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
 
-			<?php if ( is_active_sidebar( 'sidebar-footer-secondary' ) ) : ?>
-				<div class="mb-12">
-					<?php get_template_part( 'template-parts/sidebar/sidebar-footer-secondary' ); ?>
-				</div>
-			<?php endif; ?>
+				<?php if ( is_active_sidebar( 'sidebar-footer-primary' ) ) : ?>
+					<div class="mb-12">
+						<?php get_template_part( 'template-parts/sidebar/sidebar-footer-primary' ); ?>
+					</div>
+				<?php endif; ?>
 
-			<?php if ( is_active_sidebar( 'sidebar-footer-tertiary' ) ) : ?>
-				<div class="mb-12">
-					<?php get_template_part( 'template-parts/sidebar/sidebar-footer-tertiary' ); ?>
-				</div>
-			<?php endif; ?>
+				<?php if ( is_active_sidebar( 'sidebar-footer-secondary' ) ) : ?>
+					<div class="mb-12">
+						<?php get_template_part( 'template-parts/sidebar/sidebar-footer-secondary' ); ?>
+					</div>
+				<?php endif; ?>
 
-			<?php if ( is_active_sidebar( 'sidebar-footer-quaternary' ) ) : ?>
-				<div class="mb-12">
-					<?php get_template_part( 'template-parts/sidebar/sidebar-footer-quaternary' ); ?>
-				</div>
-			<?php endif; ?>
+				<?php if ( is_active_sidebar( 'sidebar-footer-tertiary' ) ) : ?>
+					<div class="mb-12">
+						<?php get_template_part( 'template-parts/sidebar/sidebar-footer-tertiary' ); ?>
+					</div>
+				<?php endif; ?>
+
+				<?php if ( is_active_sidebar( 'sidebar-footer-quaternary' ) ) : ?>
+					<div class="mb-12">
+						<?php get_template_part( 'template-parts/sidebar/sidebar-footer-quaternary' ); ?>
+					</div>
+				<?php endif; ?>
+			</div>
 		</div>
-	</div>
 
-	<div class="container">
-		<div class="mt-6 py-9 border-t border-septenary lg:flex lg:items-center lg:justify-between lg:space-x-9 lg:pt-9">
-			<div class="mb-6 lg:mb-0">
-				<p class="text-center lg:text-left text-quaternary text-sm lg:text-xs">
-					<?php echo __( 'All rights reserved - © copyright', 'ground' ) . ' ' . date( 'Y' ) . ' '; ?>
-					<?php echo GROUND_COMPANY_NAME ? GROUND_COMPANY_NAME . ' - ' : null; ?>
-					<?php echo GROUND_COMPANY_ADDRESS ? GROUND_COMPANY_ADDRESS . ' - ' : null; ?>
-					<?php echo GROUND_COMPANY_ZIP_CODE ? GROUND_COMPANY_ZIP_CODE : null; ?> <?php echo GROUND_COMPANY_CITY ? GROUND_COMPANY_CITY : null; ?>
-					<?php echo GROUND_COMPANY_PROVINCE ? ( '( ' . GROUND_COMPANY_PROVINCE . ' )' ) : null; ?>
-					<?php echo GROUND_COMPANY_COUNTRY ? GROUND_COMPANY_COUNTRY : null; ?>
-					<?php echo GROUND_COMPANY_VAT ? ' - P.IVA:' . GROUND_COMPANY_VAT : null; ?>
-					<?php echo GROUND_COMPANY_FISCAL_CODE ? ' - C.F.:' . GROUND_COMPANY_FISCAL_CODE : null; ?>
-				</p>
+		<div class="relative">
+			<div class="mt-6 py-9 border-t border-septenary lg:flex lg:items-center lg:justify-between lg:space-x-9 lg:pt-9">
+				<div class="mb-6 lg:mb-0">
+					<p class="text-center lg:text-left text-quaternary text-sm lg:text-xs">
+						<?php echo __( 'All rights reserved - © copyright', 'ground' ) . ' ' . date( 'Y' ) . ' '; ?>
+						<?php echo GROUND_COMPANY_NAME ? GROUND_COMPANY_NAME . ' - ' : null; ?>
+						<?php echo GROUND_COMPANY_ADDRESS ? GROUND_COMPANY_ADDRESS . ' - ' : null; ?>
+						<?php echo GROUND_COMPANY_ZIP_CODE ? GROUND_COMPANY_ZIP_CODE : null; ?> <?php echo GROUND_COMPANY_CITY ? GROUND_COMPANY_CITY : null; ?>
+						<?php echo GROUND_COMPANY_PROVINCE ? ( '( ' . GROUND_COMPANY_PROVINCE . ' )' ) : null; ?>
+						<?php echo GROUND_COMPANY_COUNTRY ? GROUND_COMPANY_COUNTRY : null; ?>
+						<?php echo GROUND_COMPANY_VAT ? ' - P.IVA:' . GROUND_COMPANY_VAT : null; ?>
+						<?php echo GROUND_COMPANY_FISCAL_CODE ? ' - C.F.:' . GROUND_COMPANY_FISCAL_CODE : null; ?>
+					</p>
+				</div>
+
+				<div class="lg:border-l border-septenary">
+					<?php get_template_part( 'template-parts/shared/socials' ); ?>
+				</div>
 			</div>
 
-			<div class="lg:border-l border-septenary">
-				<?php get_template_part( 'template-parts/shared/socials' ); ?>
-			</div>
 		</div>
 
 	</div>
