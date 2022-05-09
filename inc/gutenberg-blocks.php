379f3818e63723e321015347e97100cd1882e66a
Matteo Tortelli <matteo.tortelli@evoluzione.agency>
2022-05-09 11:50:34 +0200

    KNTMAIN-357: Gestione div wrapper

--- inc/gutenberg-blocks.php
+++ inc/gutenberg-blocks.php
@@ -33,37 +33,53 @@ if ( is_admin() ) {
  */
 function ground_wp_blocks_handle_custom_class( string $block_content, array $block ) {
 
+	$block_name = $block['blockName'];
+	// Skip empty blocks
+	if( !$block_name ) return $block_content;
+
 	$has_class     = strpos( $block_content, 'class="' );
 	$is_fullscreen = strpos( $block_content, 'is-fullscreen' );
 
-	/**
-	 * Paragraph
-	 */
-	if ( $block['blockName'] === 'core/paragraph' ) {
-		$block_content = ground_wp_blocks_add_custom_class( $block_content, $has_class, 'wp-block-paragraph' );
-	}
+	switch ($block_name) {
+		case 'core/paragraph':
+			$block_content = ground_wp_blocks_add_custom_class( $block_content, $has_class, 'wp-block-paragraph' );
+			break;
 
-	/**
-	 * Heading
-	 */
-	if ( $block['blockName'] === 'core/heading' ) {
-		$block_content = ground_wp_blocks_add_custom_class( $block_content, $has_class, 'wp-block-heading' );
-	}
+		case 'core/heading':
+			$block_content = ground_wp_blocks_add_custom_class( $block_content, $has_class, 'wp-block-heading' );
+			break;
 
-	/**
-	 * List
-	 */
-	if ( $block['blockName'] === 'core/list' ) {
-		$block_content = ground_wp_blocks_add_custom_class( $block_content, $has_class, 'wp-block-list' );
+		case 'core/list':
+			$block_content = ground_wp_blocks_add_custom_class( $block_content, $has_class, 'wp-block-list' );
+			break;
+		
+		default:
+			break;
 	}
+	
+	if ( is_block_to_wrap($block_name) ) {
+		$additional_classes = 'wp-block';
+		$additional_classes .= $is_fullscreen ? ' is-fullscreen' : '';
 
-	$additional_classes = $is_fullscreen ? ' is-fullscreen' : '';
-	$block_content      = '<div class="wp-block' . $additional_classes . '">' . $block_content . '</div>';
+		$block_content      = '<div class="' . $additional_classes . '">' . $block_content . '</div>';
+	}
 
 	return $block_content;
 }
 
 add_filter( 'render_block', 'ground_wp_blocks_handle_custom_class', 10, 2 );
+
+/**
+ * Function to determine if wrap the block with custom div
+ *
+ * @param string $block_name
+ * @return boolean
+ */
+function is_block_to_wrap( string $block_name ) {
+	// Avoid to wrap native blocks because of style e.g. columns
+	$blocks_black_list = array('core/column');
+	return ! in_array( $block_name , $blocks_black_list );
+}
 
 /**
  * Function to add a custom class to native wp blocks
