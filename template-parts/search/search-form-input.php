4beec3668a97da81118bc8d13ae56c7e3fb7d4e5
Matteo Tortelli <matteo.tortelli@evoluzione.agency>
2022-06-22 11:18:31 +0200

    KNTMAIN-385: Fix layout search

--- template-parts/search/search-form-input.php
+++ template-parts/search/search-form-input.php
@@ -1,8 +1,8 @@
 <form id="js-ajax-search" class="form search-form__form relative <?php echo GROUND_HEADER_TYPE == 'megaMenu' ? 'container' : ''; ?> <?php echo ( GROUND_HEADER_ADVICE_PRIMARY || GROUND_COMPANY_PHONE || GROUND_COMPANY_WHATSAPP ) ? '' : 'mt-2 lg:mt-0'; ?>" method="get" action="<?php echo home_url( '/' ); ?>">
-	<div class="flex items-center">
+	<div class="flex items-center relative">
 		<input class="shrink" type="text" id="js-ajax-search-input" placeholder="<?php _e( 'Search products', 'ground' ); ?>" value="<?php the_search_query(); ?>" name="s" id="s" />
 		<input type="hidden" name="post_type" value="product" />
 		<button class="search-form__button" type="submit"><?php ground_icon( 'search', 'form__icon' ); ?></button>
-		<div class="js-ajax-search-spinner search-form__spinner w-24"><?php ground_icon( 'spinner', 'animate-spin text-primary h-8 lg:h-12' ); ?></div>
+		<div class="js-ajax-search-spinner search-form__spinner"><?php ground_icon( 'spinner', 'animate-spin text-primary h-8 lg:h-12' ); ?></div>
 	</div>
 </form>
