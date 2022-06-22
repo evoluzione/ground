7b7a38f65dd1a14311fb24905ff88bc25a8becd3
Matteo Tortelli <matteo.tortelli@evoluzione.agency>
2022-06-22 09:47:52 +0200

    KNTMAIN-385: Separare lo script del mega menu dalla gestione del drilldown

--- assets/js/components/mega-menu.js
+++ assets/js/components/mega-menu.js
@@ -6,74 +6,59 @@ export default class MegaMenu {
 	 */
 	constructor(element) {
 		this.element = element || '.navigation__item.has-children';
-		this.defaults = {
-			triggers: this.element,
-			level: 0
-		};
 
 		this.DOM = {
 			element: document.querySelectorAll(this.element),
-			html: document.querySelector('html'),
-			back: document.querySelectorAll('.js-back'),
-			navicon: document.querySelector('.js-navicon'),
-			menuBody: document.querySelector('.js-menu-body'),
-			menuContainer: document.querySelector('.js-menu-container'),
-			closeOverlayPanel: document.querySelector('.js-close-overlay-panel'),
-			closePanel: document.querySelector('.js-close-panel'),
-			menuPanel: document.querySelector('.js-navigation-panel')
+			html: document.querySelector('html')
 		};
 
 		// window.addEventListener('DOMContentLoaded', () => {
 		this.init();
 		// });
 
-		window.addEventListener('resize', () => {
-			console.log('resizing');
-			if (isMobile().any) {
-				this.init();
-			}
-		});
+		/**
+		 * isMobile detect the user agent and not the width
+		 */
+		if (!isMobile().any) this.init();
+	}
+
+	init() {
+		if (this.DOM.element.length === 0) return;
+		this.activateHoverNavigation();
 	}
 
 	/**
 	 * Attiva l'hover nella navigation header del mega menu (solo desktop)
 	 */
 	activateHoverNavigation() {
+		this.DOM.element.forEach((item) => {
+			let timerHandle = null;
 
-		if (window.matchMedia('(min-width:1024px)').matches) {
+			item.addEventListener('mouseenter', () => {
+				clearTimeout(timerHandle);
 
-			this.DOM.element.forEach((item) => {
-				let timerHandle = null;
+				const isDesktop = window.matchMedia('(min-width:1024px)').matches;
+				if (!isDesktop) return;
 
-				item.addEventListener('mouseenter', () => {
-					clearTimeout(timerHandle);
-					this.DOM.html.classList.add('is-navigation-hover');
-					timerHandle = setTimeout(() => {
-						item.classList.add('is-hover');
-					}, 200);
-				});
-
-				item.addEventListener('mouseleave', () => {
-					clearTimeout(timerHandle);
-					this.DOM.html.classList.remove('is-navigation-hover');
-					if (item.classList.contains('is-hover')) {
-						timerHandle = setTimeout(() => {
-							item.classList.remove('is-hover');
-						}, 200);
-					}
-				});
-
+				this.DOM.html.classList.add('is-navigation-hover');
+				timerHandle = setTimeout(() => {
+					item.classList.add('is-hover');
+				}, 200);
 			});
-		}
 
+			item.addEventListener('mouseleave', () => {
+				clearTimeout(timerHandle);
+
+				const isDesktop = window.matchMedia('(min-width:1024px)').matches;
+				if (!isDesktop) return;
+
+				this.DOM.html.classList.remove('is-navigation-hover');
+				if (item.classList.contains('is-hover')) {
+					timerHandle = setTimeout(() => {
+						item.classList.remove('is-hover');
+					}, 200);
+				}
+			});
+		});
 	}
-
-	init() {
-
-		if (this.DOM.element.length === 0) return;
-
-		this.activateHoverNavigation();
-
-	}
-
 }
