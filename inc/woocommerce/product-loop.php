09aa7114fa40f95d7f492f7708ff144b21a89f3f
Matteo Tortelli <matteo.tortelli@evoluzione.agency>
2022-06-22 12:56:49 +0200

    Fix visibility filter shop btn

--- inc/woocommerce/product-loop.php
+++ inc/woocommerce/product-loop.php
@@ -115,12 +115,14 @@ function ground_archive_filters_buttons() {
 	<div class="sticky top-16 bg-quinary border-b border-septenary z-20 transform -translate-x-2/4 w-screen ml-1/2 lg:relative lg:bg-transparent lg:ml-auto lg:translate-x-0 lg:w-auto lg:border-0 lg:top-0">
 		<div class="container px-6 lg:px-0 overflow-hidden">
 			<div class="flex flex-wrap pt-3 lg:pt-0">
-				<div class="w-1/2 lg:w-2/3 pb-3 lg:pb-0 pr-3">
+				<div class="w-1/2 gap-6 lg:w-2/3 pb-3 lg:pb-0">
+					<?php if(is_active_sidebar( 'sidebar-woocommerce' )): ?>
 					<button class="button button--small button--bordered button--full-width block lg:hidden js-toggle" data-toggle-target=".sidebar--woocommerce html" data-toggle-class-name="is-sidebar-open">
-						<?php ground_icon( 'options', 'button__icon' ); ?> <?php _e( 'Filters', 'ground' ); ?>
+						<?php ground_icon( 'options', 'button__icon w-6 h-6' ); ?> <?php _e( 'Filters', 'ground' ); ?>
 					</button>
+					<?php endif; ?>
 				</div>
-				<div class="w-1/2 lg:w-1/3 pl-3">
+				<div class="w-1/2 lg:w-1/3 pb-3 pl-3">
 					<?php $result = woocommerce_catalog_ordering(); ?>
 				</div>
 				<div class="w-full col-span-full lg:order-first">
