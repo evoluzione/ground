03e536664792ed64019239a0f4e75ef6fd4dd0e0
Matteo Tortelli <matteo.tortelli@evoluzione.agency>
2022-06-29 10:16:56 +0200

    KNTMAIN-385: Replace walker with filter hook

--- functions.php
+++ functions.php
@@ -18,6 +18,7 @@ require_once 'inc/extend.php';
 require_once 'inc/customizer.php';
 require_once 'inc/head-output.php';
 require_once 'inc/navigations.php';
+require_once 'inc/navigations-structure.php';
 require_once 'inc/shortcode.php';
 require_once 'inc/walker.php';
 require_once 'inc/gutenberg-blocks.php';
