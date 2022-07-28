fc66670d804ca7e3fb5e1c0e3b795a9a6c4c9b55
Fabio Quarantini <fabio.quarantini@evoluzionetelematica.it>
2022-07-28 12:29:24 +0200

    Update modal to open video/embed

--- assets/js/components/modal.js
+++ assets/js/components/modal.js
@@ -29,7 +29,47 @@ export default class Modal {
 		this.lightbox = new PhotoSwipeLightbox({
 			gallery: this.element, // Element, NodeList, or CSS selector (string)
 			children: 'a',
+			bgOpacity: 0.9,
 			pswpModule: () => import('photoswipe')
+		});
+
+		this.lightbox.on('contentLoad', (e) => {
+			const { content } = e;
+
+			if (content.type === 'embed') {
+				e.preventDefault();
+
+				content.element = document.createElement('div');
+				content.element.className = 'pswp__embed-container';
+
+				const iframe = document.createElement('iframe');
+				iframe.setAttribute('allowfullscreen', '');
+				iframe.setAttribute('frameborder', '0');
+				iframe.setAttribute(
+					'allow',
+					'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+				);
+				iframe.src = content.data.src;
+				content.element.appendChild(iframe);
+			}
+
+			if (content.type === 'video') {
+				e.preventDefault();
+
+				content.element = document.createElement('div');
+				content.element.className = 'pswp__video-container';
+
+				const video = document.createElement('video');
+				video.className = 'pswp__video';
+				video.setAttribute('loop', '');
+				video.setAttribute('controls', '');
+				video.setAttribute('autoplay', '');
+				content.element.appendChild(video);
+
+				const videoSource = document.createElement('source');
+				videoSource.src = content.data.src;
+				video.appendChild(videoSource);
+			}
 		});
 
 		this.lightbox.init();
