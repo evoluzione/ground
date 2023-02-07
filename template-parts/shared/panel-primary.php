733521ca834cee97aacbc3fbc0b453cac0d36838
Valerio Acerbis <valerio.acerbis@evoluzionetelematica.it>
2023-02-07 10:10:34 +0100

    minor fix panel scroll all category + 2.0.14

--- template-parts/shared/panel-primary.php
+++ template-parts/shared/panel-primary.php
@@ -11,7 +11,7 @@
 			<?php get_template_part( 'template-parts/shared/logo-primary' ); ?>
 		</div>
 
-		<div class="overlay-panel__content">
+		<div class="overlay-panel__content lg:pb-52">
 			<button type="button" class="js-back block mb-6 header__back cursor-pointer ml-8"> 
 				<span> <?php ground_icon( 'chevron-left', 'text-tertiary h-6 w-6' ); ?> </span> <?php _e( 'Back', 'ground' ); ?>
 			</button>
