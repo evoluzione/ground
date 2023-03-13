0bcbf6933ea3de9e33cad4754cc1d284a0cf4c56
Valerio Acerbis <valerio.acerbis@evoluzionetelematica.it>
2023-03-13 10:03:09 +0100

    Refactoring Sidebar + Sidebar Avanzata

--- inc/woocommerce/product-loop.php
+++ inc/woocommerce/product-loop.php
@@ -113,16 +113,16 @@ function ground_archive_filters_buttons() {
 	?>
 
 	<div class="sticky top-16 bg-quinary border-b border-septenary z-10 transform -translate-x-2/4 w-screen ml-1/2 lg:relative lg:bg-transparent lg:ml-auto lg:translate-x-0 lg:w-auto lg:border-0 lg:top-0">
-		<div class="container px-3 lg:px-0 overflow-hidden">
+		<div class="px-0 overflow-hidden">
 			<div class="flex flex-wrap pt-3 lg:pt-0">
-				<div class="w-1/2 gap-6 lg:w-2/3 sm:pr-3 pb-3 lg:pb-0 pl-3 lg:pl-0">
+				<div class="w-1/2 gap-6 lg:w-2/3 sm:pr-3 pb-3 lg:pb-0 pl-6 lg:pl-0 pr-3 lg:pr-0">
 					<?php if ( is_active_sidebar( 'sidebar-woocommerce' ) || is_active_sidebar( 'sidebar-woocommerce-advanced' ) ) : ?>
 						<button class="button button--bordered button--full-width block lg:hidden js-toggle" data-toggle-target="#sidebar-woocommerce html" data-toggle-class-name="is-sidebar-open">
 							<?php ground_icon( 'options', 'button__icon w-6 h-6' ); ?> <?php _e( 'Filters', 'ground' ); ?>
 						</button>
 					<?php endif; ?>
 				</div>
-				<div class="w-1/2 lg:w-1/3 pb-3 pl-3 pr-3 lg:pr-0">
+				<div class="w-1/2 lg:w-1/3 pb-3 pl-3 pr-6 lg:pr-0">
 					<?php $result = woocommerce_catalog_ordering(); ?>
 				</div>
 				<div class="w-full col-span-full lg:order-first">
@@ -136,7 +136,6 @@ function ground_archive_filters_buttons() {
 						the_widget( 'WC_Widget_Layered_Nav_Filters' );
 					}
 					?>
-
 				</div>
 			</div>
 		</div>
@@ -189,8 +188,10 @@ function ground_add_sidebar_woocommerce() {
 				<div class="sidebar__content">
 					<?php dynamic_sidebar( 'sidebar-woocommerce' ); ?>
 					<?php if ( is_active_sidebar( 'sidebar-woocommerce-advanced' ) ) : ?>
-						<div class="button button--bordered w-full js-toggle" data-toggle-target="#overlay-panel-filter-woocommerce-advanced html" data-toggle-class-name="is-overlay-panel-open"><?php ground_icon( 'options', 'button__icon' ); ?> <?php _e( 'Advanced Filters', 'ground' ); ?></div>
-						<?php dynamic_sidebar( 'sidebar-woocommerce-advanced' ); ?>
+						<div class="button button--bordered w-full js-toggle" data-toggle-target="#ground-sidebar-woocommerce-advanced" data-toggle-class-name="hidden"><?php ground_icon( 'options', 'button__icon' ); ?> <?php _e( 'Advanced Filters', 'ground' ); ?></div>
+						<div class="hidden" id="ground-sidebar-woocommerce-advanced">
+							<?php dynamic_sidebar( 'sidebar-woocommerce-advanced' ); ?>
+						</div>
 						<?php endif; ?>
 				</div>
 				<div class="sidebar__close js-toggle" data-toggle-target="#sidebar-woocommerce html" data-toggle-class-name="is-sidebar-open"><?php ground_icon( 'close' ); ?></div>
