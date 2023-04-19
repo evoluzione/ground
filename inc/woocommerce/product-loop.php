fe85059fd5886d3b3205fb7121a4ae47715e1e4b
Valerio Acerbis <valerio.acerbis@evoluzionetelematica.it>
2023-04-19 16:11:24 +0200

    minor fix sidebar + 2.0.31

--- inc/woocommerce/product-loop.php
+++ inc/woocommerce/product-loop.php
@@ -139,27 +139,7 @@ function ground_add_sidebar_horizontal_woocommerce() {
  * WooCommerce, Add Sidebar Woocommerce
  */
 function ground_add_sidebar_woocommerce() {
-	if ( is_active_sidebar( 'sidebar-woocommerce' ) ) :
-		?>
-
-		<div class="sidebar" id="sidebar-woocommerce">
-			<div class="sidebar__mask js-toggle" data-toggle-target="#sidebar-woocommerce html" data-toggle-class-name="is-sidebar-open"></div>
-			<div class="sidebar__body">
-				<div class="sidebar__content">
-					<?php dynamic_sidebar( 'sidebar-woocommerce' ); ?>
-					<?php if ( is_active_sidebar( 'sidebar-woocommerce-advanced' ) ) : ?>
-						<div class="button button--bordered w-full js-toggle" data-toggle-target="#ground-sidebar-woocommerce-advanced" data-toggle-class-name="hidden"><?php ground_icon( 'options', 'button__icon' ); ?> <?php _e( 'Advanced Filters', 'ground' ); ?></div>
-						<div class="hidden" id="ground-sidebar-woocommerce-advanced">
-							<?php dynamic_sidebar( 'sidebar-woocommerce-advanced' ); ?>
-						</div>
-						<?php endif; ?>
-				</div>
-				<div class="sidebar__close js-toggle" data-toggle-target="#sidebar-woocommerce html" data-toggle-class-name="is-sidebar-open"><?php ground_icon( 'close' ); ?></div>
-			</div>
-		</div>
-
-		<?php
-	endif;
+	get_template_part( 'template-parts/sidebar/sidebar-woocommerce' );
 }
 
 add_action( 'woocommerce_sidebar', 'ground_add_sidebar_woocommerce', 10 );
--- template-parts/sidebar/sidebar-archive-post.php
+++ template-parts/sidebar/sidebar-archive-post.php
@@ -1,9 +1,11 @@
-<div class="sidebar sidebar--archive-post">
+<?php if ( is_active_sidebar( 'sidebar-archive-post' ) ) : ?>
+<div class="sidebar" id="sidebar-archive-post">
+	<div class="sidebar__mask js-toggle" data-toggle-target="#sidebar-archive-post html" data-toggle-class-name="is-sidebar-open"></div>
 	<div class="sidebar__body">
-		<?php dynamic_sidebar('sidebar-archive-post'); ?>
-		<div class="mt-6 border-t block border-septenary">
-			<a class="text-base mb-3 lg:mb-6 mt-6 inline-block" href="<?php echo get_post_type_archive_link('post'); ?>"> <span class="text-sm"><</span> <?php _e('Back to ', 'ground'); ?> <?php echo get_the_title(get_option('page_for_posts', true)) ?></a>
+		<div class="sidebar__content">
+			<?php dynamic_sidebar( 'sidebar-archive-post' ); ?>
 		</div>
+		<div class="sidebar__close js-toggle" data-toggle-target="#sidebar-archive-post html" data-toggle-class-name="is-sidebar-open"><?php ground_icon( 'close' ); ?></div>
 	</div>
-	<div class="sidebar__close js-toggle" data-toggle-target=".sidebar--archive-post html" data-toggle-class-name="is-sidebar-open"><?php ground_icon('close'); ?></div>
 </div>
+<?php endif; ?>
