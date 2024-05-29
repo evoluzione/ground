4c197fb7f9ab60a9ae12b4b7733598bcfad3c1dd
Fabio Quarantini <wlospam@gmail.com>
2024-05-29 16:47:44 +0200

    Aggiorna formattazioni

--- template-parts/content/content-post.php
+++ template-parts/content/content-post.php
@@ -1,14 +1,14 @@
 <main>
 	<article class="container">
 
-		<header>
+		<header class="mb-6">
 			<h1 class="text-4xl"><?php the_title(); ?></h1>
 		</header>
 
 		<?php ground_image( [ 
 			'size' => '16-9-large',
 			'attr' => [ 
-				'class' => 'aspect-[16/9] object-cover w-full',
+				'class' => 'aspect-[16/9] object-cover w-full mb-6',
 				'alt' => get_the_title(),
 			],
 		] ); ?>
