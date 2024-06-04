652a1bd5482276141efefa764e68af992ec15144
Fabio Quarantini <wlospam@gmail.com>
2024-06-04 12:11:55 +0200

    Update page layout

--- template-parts/content/content-page.php
+++ template-parts/content/content-page.php
@@ -1,22 +1,20 @@
-<main>
-	<article class="container">
+<article>
 
-		<header class="mb-6">
-			<h1 class="text-4xl"><?php the_title(); ?></h1>
-		</header>
+	<header class="mb-6">
+		<h1 class="text-4xl"><?php the_title(); ?></h1>
+	</header>
 
-		<?php ground_image( [ 
-			'size' => '16-9-large',
-			'attr' => [ 
-				'class' => 'aspect-[16/9] object-cover w-full mb-6',
-				'alt' => get_the_title(),
-			],
-			'default_image' => '',
-		] ); ?>
+	<?php ground_image( [ 
+		'size' => '16-9-large',
+		'attr' => [ 
+			'class' => 'aspect-[16/9] object-cover w-full mb-6',
+			'alt' => get_the_title(),
+		],
+		'default_image' => '',
+	] ); ?>
 
-		<div class="prose">
-			<?php the_content(); ?>
-		</div>
+	<div class="prose">
+		<?php the_content(); ?>
+	</div>
 
-	</article> <!-- End .container -->
-</main>
+</article>
--- templates/page.php
+++ templates/page.php
@@ -5,14 +5,22 @@
  * @package Ground
  */
 
-get_template_part( 'template-parts/header/header' );
+get_template_part( 'template-parts/header/header' ); ?>
 
-while ( have_posts() ) :
+<main class="container grid grid-cols-12 gap-6">
+	<div class="col-span-2">
+		<?php get_template_part( 'template-parts/sidebar/sidebar-primary' ); ?>
+	</div>
 
-	the_post();
+	<div class="col-span-10">
+		<?php while ( have_posts() ) :
 
-	get_template_part( 'template-parts/content/content-page' );
+			the_post();
 
-endwhile;
+			get_template_part( 'template-parts/content/content-page' );
 
-get_template_part( 'template-parts/footer/footer' );
+		endwhile; ?>
+	</div>
+</main>
+
+<?php get_template_part( 'template-parts/footer/footer' );
