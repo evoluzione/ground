bd4e41b2bd674bffb7d77bc96e13fe173d6a917a
Fabio Quarantini <wlospam@gmail.com>
2024-10-01 15:47:20 +0200

    Rimuove margini liste nel prose

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,4 +5,20 @@ export default {
 	presets: [
 		require('@evoluzione/tailwind-config'),
 	],
+	theme: {
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            'ul li': {
+              margin: '0',
+            },
+            'ol li': {
+              margin: '0',
+            },
+          },
+        },
+      },
+    },
+  },
 };
