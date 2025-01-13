cca838db0a91a2b4c0a30b48544587784b8c0cf4
Fabio Quarantini <wlospam@gmail.com>
2025-01-13 19:01:33 +0100

    Aggiunge commenti per il loder

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ import { scrollDirection } from './components/scrollDirection.js';
 
 (() => {
 
+	// Load
 	window.addEventListener('load', function() {
 		document.documentElement.classList.remove('is-loading');
 		document.documentElement.classList.add('is-loaded');
