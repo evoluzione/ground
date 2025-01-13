9e861c7149ce516afac9cdcf2956b3c5723ca6f4
Fabio Quarantini <wlospam@gmail.com>
2025-01-13 19:00:29 +0100

    Rimuove la classe 'has-no-js' e gestisce il caricamento della pagina con classi 'is-loading' e 'is-loaded'

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,11 @@ import { scrollDirection } from './components/scrollDirection.js';
 
 (() => {
 
+	window.addEventListener('load', function() {
+		document.documentElement.classList.remove('is-loading');
+		document.documentElement.classList.add('is-loaded');
+	});
+
 	// Toggle
 	if (document.querySelector('.js-toggle')) {
 		import('./utilities/toggle.js').then(({default: Toggle}) => {
--- template-parts/header/header-primary.php
+++ template-parts/header/header-primary.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html <?php language_attributes(); ?> class="has-no-js is-loading">
+<html <?php language_attributes(); ?> class="is-loading">
 
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
