88ad7a41093c5cd98988f04791d5451dbbe6caa7
Fabio Quarantini <wlospam@gmail.com>
2025-05-05 11:44:04 +0200

    refactor: enhance navigation sidebar with additional class options for items and links

--- template-parts/navigation/navigation-sidebar-primary.php
+++ template-parts/navigation/navigation-sidebar-primary.php
@@ -1,11 +1,16 @@
 <nav>
 	<?php ground_subpages( [ 
 		'merge_classes' => false,
-		// 'menu_class' => '',
+		'menu_class' => '',
 		'submenu_class' => 'pl-6',
+		'submenu_class_1' => '',
 		'item_class' => '',
-		// 'item_active_class' => '',
+		'item_class_1' => '',
+		'item_active_class' => '',
+		'item_active_class_1' => '',
 		'link_class' => 'hover:text-primary',
+		'link_class_1' => '',
 		'link_active_class' => 'text-primary',
+		'link_active_class_1' => '',
 	] ); ?>
 </nav>
