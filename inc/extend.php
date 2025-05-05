4217eee639913652f1cf59b5417952bc0903db33
Fabio Quarantini <wlospam@gmail.com>
2025-05-05 10:11:27 +0200

    refactor: enhance nav menu class handling with merge_classes support

--- inc/extend.php
+++ inc/extend.php
@@ -2,7 +2,7 @@
 /**
  * Localization
  *
- * Load the theme’s translated strings
+ * Load the theme's translated strings
  */
 function ground_load_theme_textdomain() {
 	load_theme_textdomain( 'ground', GROUND_TEMPLATE_DIRECTORY . '/languages' );
@@ -61,25 +61,21 @@ add_filter( 'get_the_archive_title', 'ground_remove_archive_title_prefixes', 10,
  *
  * @param array    $classes    An array of the CSS classes that are applied to the menu item's <li> element.
  * @param WP_Post  $menu_item  The current menu item.
- * @param stdClass $args       An object of wp_nav_menu() arguments.
+ * @param stdClass $args       An object of wp_nav_menu() arguments. Supports 'merge_classes' (bool).
  * @param int      $depth      Depth of menu item. Used for padding.
  * @return array   Modified array of CSS classes.
  */
 function ground_nav_menu_css_class( $classes, $menu_item, $args, $depth ) {
-
 	$depth_key = $depth + 1;
-
 	$remove_default_class = $args->remove_default_class ?? false;
+	$merge_classes = $args->merge_classes ?? false;
 
 	$item_class = $args->item_class ?? '';
 	$item_class_depth = $args->{'item_class_' . $depth_key} ?? '';
-
 	$item_active_class = $args->item_active_class ?? '';
 	$item_active_class_depth = $args->{'item_active_class_' . $depth_key} ?? '';
-
 	$item_parent_class = $args->item_parent_class ?? '';
 	$item_parent_class_depth = $args->{'item_parent_class_' . $depth_key} ?? '';
-
 	$item_ancestor_class = $args->item_ancestor_class ?? '';
 	$item_ancestor_class_depth = $args->{'item_ancestor_class_' . $depth_key} ?? '';
 
@@ -89,42 +85,50 @@ function ground_nav_menu_css_class( $classes, $menu_item, $args, $depth ) {
 		$classes = array_diff( $classes, $remove_default_class );
 	}
 
-	if ( $item_class ) {
-		$classes[] = $item_class;
+	if ( $merge_classes ) {
+		if ( $menu_item->current ) {
+			$classes[] = $item_active_class_depth ?: $item_active_class;
+		} elseif ( $menu_item->current_item_parent ) {
+			$classes[] = $item_parent_class_depth ?: $item_parent_class;
+		} elseif ( $menu_item->current_item_ancestor ) {
+			$classes[] = $item_ancestor_class_depth ?: $item_ancestor_class;
+		} else {
+			$classes[] = $item_class_depth ?: $item_class;
+		}
+	} else {
+		if ( $item_class ) {
+			$classes[] = $item_class;
+		}
+		if ( $item_class_depth ) {
+			$classes[] = $item_class_depth;
+		}
+		if ( $menu_item->current ) {
+			if ( $item_active_class ) {
+				$classes[] = $item_active_class;
+			}
+			if ( $item_active_class_depth ) {
+				$classes[] = $item_active_class_depth;
+			}
+		}
+		if ( $menu_item->current_item_parent ) {
+			if ( $item_parent_class ) {
+				$classes[] = $item_parent_class;
+			}
+			if ( $item_parent_class_depth ) {
+				$classes[] = $item_parent_class_depth;
+			}
+		}
+		if ( $menu_item->current_item_ancestor ) {
+			if ( $item_ancestor_class ) {
+				$classes[] = $item_ancestor_class;
+			}
+			if ( $item_ancestor_class_depth ) {
+				$classes[] = $item_ancestor_class_depth;
+			}
+		}
 	}
 
-	if ( $item_class_depth ) {
-		$classes[] = $item_class_depth;
-	}
-
-	if ( $menu_item->current ) {
-		if ( $item_active_class ) {
-			$classes[] = $item_active_class;
-		}
-		if ( $item_active_class_depth ) {
-			$classes[] = $item_active_class_depth;
-		}
-	}
-
-	if ( $menu_item->current_item_parent ) {
-		if ( $item_parent_class ) {
-			$classes[] = $item_parent_class;
-		}
-		if ( $item_parent_class_depth ) {
-			$classes[] = $item_parent_class_depth;
-		}
-	}
-
-	if ( $menu_item->current_item_ancestor ) {
-		if ( $item_ancestor_class ) {
-			$classes[] = $item_ancestor_class;
-		}
-		if ( $item_ancestor_class_depth ) {
-			$classes[] = $item_ancestor_class_depth;
-		}
-	}
-
-	return $classes;
+	return array_filter( array_unique( $classes ) );
 }
 
 add_filter( 'nav_menu_css_class', 'ground_nav_menu_css_class', 1, 4 );
@@ -133,15 +137,16 @@ add_filter( 'nav_menu_css_class', 'ground_nav_menu_css_class', 1, 4 );
  * Filters the CSS classes applied to a submenu.
  *
  * @param array    $classes An array of the CSS classes that are applied to the submenu's <ul> element.
- * @param stdClass $args    An object of wp_nav_menu() arguments.
+ * @param stdClass $args    An object of wp_nav_menu() arguments. Supports 'merge_classes' (bool).
  * @param int      $depth   Depth of menu item. Used for padding.
  * @return array   Modified array of CSS classes.
  */
 function ground_nav_menu_submenu_css_class( $classes, $args, $depth ) {
-
 	$remove_default_class = $args->remove_default_class ?? false;
+	$merge_classes = $args->merge_classes ?? false;
+	$depth_key = $depth + 1;
 	$submenu_class = $args->submenu_class ?? '';
-	$submenu_class_depth = $args->{'submenu_class_' . $depth + 1} ?? '';
+	$submenu_class_depth = $args->{'submenu_class_' . $depth_key} ?? '';
 
 	if ( $remove_default_class === true ) {
 		$classes = array();
@@ -149,15 +154,18 @@ function ground_nav_menu_submenu_css_class( $classes, $args, $depth ) {
 		$classes = array_diff( $classes, $remove_default_class );
 	}
 
-	if ( $submenu_class ) {
-		$classes[] = $submenu_class;
+	if ( $merge_classes ) {
+		$classes[] = $submenu_class_depth ?: $submenu_class;
+	} else {
+		if ( $submenu_class ) {
+			$classes[] = $submenu_class;
+		}
+		if ( $submenu_class_depth ) {
+			$classes[] = $submenu_class_depth;
+		}
 	}
 
-	if ( $submenu_class_depth ) {
-		$classes[] = $submenu_class_depth;
-	}
-
-	return $classes;
+	return array_filter( array_unique( $classes ) );
 }
 
 add_filter( 'nav_menu_submenu_css_class', 'ground_nav_menu_submenu_css_class', 10, 3 );
@@ -167,7 +175,7 @@ add_filter( 'nav_menu_submenu_css_class', 'ground_nav_menu_submenu_css_class', 1
  *
  * @param array    $atts   The HTML attributes applied to the menu item's <a> element, empty strings are ignored.
  * @param WP_Post  $item   The current menu item.
- * @param stdClass $args   An object of wp_nav_menu() arguments.
+ * @param stdClass $args   An object of wp_nav_menu() arguments. Supports 'merge_classes' (bool).
  * @param int      $depth  Depth of menu item. Used for padding.
  * @return array   Modified array of HTML attributes.
  */
@@ -175,72 +183,74 @@ function ground_nav_menu_link_css_class( $atts, $item, $args, $depth ) {
 	if ( ! is_array( $atts ) ) {
 		$atts = [];
 	}
-
 	if ( ! is_object( $args ) || ! is_object( $item ) ) {
 		return $atts;
 	}
-
 	$depth_key = $depth + 1;
-
 	$remove_default_class = $args->remove_default_class ?? false;
-
+	$merge_classes = $args->merge_classes ?? false;
 	$link_class = $args->link_class ?? '';
 	$link_class_depth = $args->{'link_class_' . $depth_key} ?? '';
-
 	$link_active_class = $args->link_active_class ?? '';
 	$link_active_class_depth = $args->{'link_active_class_' . $depth_key} ?? '';
-
 	$link_parent_class = $args->link_parent_class ?? '';
 	$link_parent_class_depth = $args->{'link_parent_class_' . $depth_key} ?? '';
-
 	$link_ancestor_class = $args->link_ancestor_class ?? '';
 	$link_ancestor_class_depth = $args->{'link_ancestor_class_' . $depth_key} ?? '';
-
 	$current_classes = ! empty( $atts['class'] ) ? explode( ' ', $atts['class'] ) : [];
-
+	
 	if ( $remove_default_class === true ) {
 		$current_classes = [];
 	} elseif ( is_array( $remove_default_class ) ) {
 		$current_classes = array_diff( $current_classes, $remove_default_class );
 	}
 
-	if ( $link_class ) {
-		$current_classes[] = $link_class;
+	$new_classes = [];
+
+	if ( $merge_classes ) {
+		if ( $item->current ) {
+			$new_classes[] = $link_active_class_depth ?: $link_active_class;
+		} elseif ( $item->current_item_parent ) {
+			$new_classes[] = $link_parent_class_depth ?: $link_parent_class;
+		} elseif ( $item->current_item_ancestor ) {
+			$new_classes[] = $link_ancestor_class_depth ?: $link_ancestor_class;
+		} else {
+			$new_classes[] = $link_class_depth ?: $link_class;
+		}
+	} else {
+		if ( $link_class ) {
+			$new_classes[] = $link_class;
+		}
+		if ( $link_class_depth ) {
+			$new_classes[] = $link_class_depth;
+		}
+		if ( $item->current ) {
+			if ( $link_active_class ) {
+				$new_classes[] = $link_active_class;
+			}
+			if ( $link_active_class_depth ) {
+				$new_classes[] = $link_active_class_depth;
+			}
+		}
+		if ( $item->current_item_parent ) {
+			if ( $link_parent_class ) {
+				$new_classes[] = $link_parent_class;
+			}
+			if ( $link_parent_class_depth ) {
+				$new_classes[] = $link_parent_class_depth;
+			}
+		}
+		if ( $item->current_item_ancestor ) {
+			if ( $link_ancestor_class ) {
+				$new_classes[] = $link_ancestor_class;
+			}
+			if ( $link_ancestor_class_depth ) {
+				$new_classes[] = $link_ancestor_class_depth;
+			}
+		}
 	}
 
-	if ( $link_class_depth ) {
-		$current_classes[] = $link_class_depth;
-	}
-
-	if ( $item->current ) {
-		if ( $link_active_class ) {
-			$current_classes[] = $link_active_class;
-		}
-		if ( $link_active_class_depth ) {
-			$current_classes[] = $link_active_class_depth;
-		}
-	}
-
-	if ( $item->current_item_parent ) {
-		if ( $link_parent_class ) {
-			$current_classes[] = $link_parent_class;
-		}
-		if ( $link_parent_class_depth ) {
-			$current_classes[] = $link_parent_class_depth;
-		}
-	}
-
-	if ( $item->current_item_ancestor ) {
-		if ( $link_ancestor_class ) {
-			$current_classes[] = $link_ancestor_class;
-		}
-		if ( $link_ancestor_class_depth ) {
-			$current_classes[] = $link_ancestor_class_depth;
-		}
-	}
-
-	$atts['class'] = esc_attr( implode( ' ', array_unique( array_filter( $current_classes ) ) ) );
-
+	$atts['class'] = esc_attr( implode( ' ', array_unique( array_filter( $new_classes ) ) ) );
 	return $atts;
 }
 
