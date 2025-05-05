5acb332f7f07a8916356abde709aa7b4badd812e
Fabio Quarantini <wlospam@gmail.com>
2025-05-05 11:35:39 +0200

    refactor: update pagination class structure to support additional customization options

--- template-parts/pagination/pagination-primary.php
+++ template-parts/pagination/pagination-primary.php
@@ -3,12 +3,31 @@
 $query = isset( $args['query'] ) ? $args['query'] : $wp_query;
 
 ground_pagination( [ 
-	'nav_class' => 'flex justify-center',
-	'list_class' => 'flex',
-	'prev_class' => 'pl-3 py-1',
-	'next_class' => 'pr-3 py-1',
-	'dots_class' => 'px-3 py-1',
-	'page_class' => 'px-3 py-1',
-	'page_active_class' => 'px-3 py-1 font-bold text-primary',
+	'mid_size' => 1,
+	'merge_classes' => false,
+
+	'container_class' => 'flex justify-center',
+	'list_class' => 'flex gap-4',
+
+	'item_class' => '',
+	'item_active_class' => '',
+
+	'item_prev_class' => '',
+	'item_next_class' => '',
+	'item_dots_class' => '',
+	'item_page_class' => '',
+
+	'link_class' => '',
+	'link_prev_class' => '',
+	'link_next_class' => '',
+	'link_page_class' => '',
+
+	'text_class' => '',
+	'text_page_class' => '',
+	'text_dots_class' => '',
+
+	'text_active_class' => '',
+
+	'item_page_active_class' => 'font-bold text-primary',
 	'total' => $query->max_num_pages
 ] );
