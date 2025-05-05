ebdaf5069efa90aaa2ae5799a259a0bfa7b03abf
Fabio Quarantini <wlospam@gmail.com>
2025-05-05 09:20:23 +0200

    chore: update comments

--- inc/extend.php
+++ inc/extend.php
@@ -67,7 +67,7 @@ add_filter( 'get_the_archive_title', 'ground_remove_archive_title_prefixes', 10,
  */
 function ground_nav_menu_css_class( $classes, $menu_item, $args, $depth ) {
 
-	$depth_key = $depth + 1; // Standardizzare l'accesso agli argomenti per profondità.
+	$depth_key = $depth + 1;
 
 	$remove_default_class = $args->remove_default_class ?? false;
 
