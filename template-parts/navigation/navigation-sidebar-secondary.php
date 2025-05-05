eef2bef62cb1061fd74c114db6ba9feaf27468b6
Fabio Quarantini <wlospam@gmail.com>
2025-05-05 11:50:52 +0200

    refactor: add additional options for ground_terms function in navigation sidebar

--- template-parts/navigation/navigation-sidebar-secondary.php
+++ template-parts/navigation/navigation-sidebar-secondary.php
@@ -1,5 +1,20 @@
-<?php
-// TODO: Inserirlo diretto senza navigation nelle sidebar? ?>
 <nav>
-	<?php ground_terms(); ?>
+	<?php
+	ground_terms( [ 
+		// 'taxonomy' => 'ground_catalog_taxonomy',
+		'merge_classes' => true,
+		// 'child_of' => get_queried_object_id(),
+		'menu_class' => 'list-disc ps-6 mb-24',
+		'submenu_class' => 'list-disc ps-6 pl-6',
+		// 'submenu_class_1' => '',
+		// 'item_class' => '',
+		// 'item_class_1' => '',
+		// 'item_active_class' => '',
+		// 'item_active_class_1' => '',
+		// 'link_class' => '',
+		// 'link_class_1' => '',
+		'link_active_class' => 'text-primary',
+		// 'link_active_class_1' => '',
+	] );
+	?>
 </nav>
